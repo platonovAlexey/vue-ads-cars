635c6b083c75118b5e6e5125f061a5520c9bf3bf
platonovAlexey <kalyawe4ka@gmail.com>
2018-05-25 16:36:27 +0300

    add order page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,33 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list two-line subheader>
+          <v-list-tile
+            avatar
+            v-for="order of orders"
+            :key="order.id"
+          >
+            <v-list-tile-action>
+              <v-checkbox
+                :input-value="order.done"
+                @change="markDone(order)"
+                color="primary"
+              ></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                :to="'/ad/' + order.adId"
+                class="primary"
+              >Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,6 +37,20 @@
   export default {
     data () {
       return {
+        orders: [
+          {
+            id: '111',
+            name: 'Alex',
+            phone: '8-999-999-99-99',
+            adId: '123',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
       }
     }
   }
